b6989a6bbeb634c619d1beb23b74dff9e6101642
ColmSam2319 <colmsammon13@gmail.com>
2022-10-13 20:13:05 +0000

    changed app setting

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-68j+vpdeb&v-z$_yu&j)mnlfj*z9-p@ib2alid2q^osi*87(@k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['colsam23-django-todo-app.herokuapp.com']
 
 
 # Application definition
